04f37cf80b5213f623fb249a99a38176fc7f5e29
Jonathan Evens <jonathan.evens@gmail.com>
2019-07-08 17:42:59 -0700

    Update normalEqn.m

--- normalEqn.m
+++ normalEqn.m
@@ -5,19 +5,7 @@ function [theta] = normalEqn(X, y)
 
 theta = zeros(size(X, 2), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the code to compute the closed form solution
-%               to linear regression and put the result in theta.
-%
-
 theta=inv(X'*X)*X'*y;
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-% -------------------------------------------------------------
-
 
 % ============================================================
 
